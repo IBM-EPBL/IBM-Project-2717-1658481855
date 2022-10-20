cfb024d7184f8ee9639b52e66719e34b859d9b9a
Deepikala2002 <104084562+Deepikala2002@users.noreply.github.com>
2022-10-20 14:54:36 +0530

    Add files via upload

---
+++ Assignments/Deepikala V/Assignment1/code/app.py
@@ -0,0 +1,19 @@
+from flask import *;
+import os
+app = Flask(_name_)
+@app.route('/', methods=['GET', 'POST'])
+def Home():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email= request.form["email"]
+        mobile = request.form["mobile"]
+        return redirect(url_for('output', name=name, email=email, mobile=mobile))
+    return render_template('Home.html')
+@app.route("/output", methods=['GET', 'POST'])
+def output():
+    name = request.form.get('name')
+    email= request.form.get('email')
+    mobile = request.form.get('mobile')
+    return render_template('output.html', name=name, email=email, mobile=mobile)
+if _name_ == "_main_":
+    app.run(debug=True, port=8080)
