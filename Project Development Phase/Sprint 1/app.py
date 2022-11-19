b0393d076cf35156867959bbe00abacabd6bf464
Anjana S <70279746+sekaranjana@users.noreply.github.com>
2022-11-19 18:24:28 +0530

    Update app.py

--- Project Development Phase/Sprint 1/app.py
+++ Project Development Phase/Sprint 1/app.py
@@ -1,71 +1,221 @@
-from flask import Flask, render_template, request, session
-import requests
-import ibm_db
-import bcrypt
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;PROTOCOL=TCPIP;UID=wdt07473;PWD=khxUTQVy0OaDAOdc",'','')
+from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(_name_)
+import requests, json, os
+import ibm_db
+import re
+
+import cv2
+
+app = Flask(__name__)
+
 app.secret_key = 'a'
+
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=qfn70649;PWD=aXek5nfjbJy0IGQV",'','')
+
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/register')
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+    global userid
+    msg=''
+
+    if request.method =='POST':
+        username = request.form['username']
+        password =request.form['password']
+        sql ="SELECT * FROM users WHERE username =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print (account)
+        if account:
+            session['loggedin']=True
+            session['id'] = account ['USERNAME']
+            userid = account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'logged in successfully !'
+
+            return render_template('submission.html',msg = msg)
+
+        else:
+            msg ='Incorrect username / password !'
+    return render_template('login.html',msg=msg)
+
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
-
-@app.route('/uploaddata', methods=['GET', 'POST'])
-def uploaddata():
     msg = ''
     if request.method == 'POST':
-        name = request.form['username']
-        email = request.form['emailaddress']
-        pword = request.form['pword']
-        
-        session["username"] = name
-           
-        cursor = mysql.connection.cursor()
-        cursor.execute(
-            'INSERT INTO user VALUES (% s, % s, % s)', (name, email, pword))
-        mysql.connection.commit()
-        msg = 'You have successfully registered !'
-    return render_template('login.html', msg=msg)
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/authenticate', methods=['GET', 'POST'])
-def authenticate():
-    if request.method == 'POST':
-        email = request.form['emailaddress']
-        pword = request.form['pword']
-        cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM user WHERE email= % s', (email,))
-        mysql.connection.commit()
-        userexist = cursor.fetchone()
-        if userexist == None:
-            msg = 'User with this Email doesn\'t exist. Please Sign-up before Login'
-            return render_template('login.html', msg=msg)
-        cursor.execute(
-            'SELECT * FROM user WHERE email= % s and password = % s', (email, pword))
-        mysql.connection.commit()
-        data = cursor.fetchone()
-        if data == None:
-            data = 'INCORRECT DETAILS'
-            return render_template('login.html', msg=data)
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        sql = "SELECT * FROM users WHERE username = ?"
+        stmt = ibm_db.prepare(conn,sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            msg ='Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+',email):
+            msg ='Invaild email address !'
+        elif not re.match(r'[A-Za-z0-9]+',username):
+            msg = 'Name must contain only characters and numbers!'
         else:
-            session["email"] = email
-        print("data", data)
-    return render_template('index.html')
+            insert_sql = "INSERT INTO users VALUES (?,?,?)"
+            prep_stmt= ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1 , username)
+            ibm_db.bind_param(prep_stmt, 2, email)
+            ibm_db.bind_param(prep_stmt, 3, password)
+            ibm_db.execute(prep_stmt)
+            msg = ' you have successfully registered !'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+@app.route('/submission')
+def submission():
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return render_template('index.html')
+    return render_template('submission.html')
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', debug=True,port=8080)
+@app.route('/pythonlogin/submission/display', methods = ["POST", "GET"])
+def display():
+    if request.method == "POST":
+
+
+
+        image = request.files["food"]
+        image.save('static/Out/Test.jpg')
+
+        import tensorflow as tf
+        classifierLoad = tf.keras.models.load_model('model.h5')
+
+        import numpy as np
+        from keras.preprocessing import image
+
+        test_image = image.load_img('static/Out/Test.jpg', target_size=(200, 200))
+        img1 = cv2.imread('static/Out/Test.jpg')
+        # test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = classifierLoad.predict(test_image)
+
+        print(result)
+
+        out = ''
+        fer = ''
+        if result[0][0] == 1:
+            out = "APPLES"
+        elif result[0][1] == 1:
+            out = "Badam"
+        elif result[0][2] == 1:
+            out = "Badam Drink"
+        elif result[0][3] == 1:
+            out = "Banana"
+        elif result[0][4] == 1:
+            out = "Beef Steak"
+        elif result[0][5] == 1:
+            out = "BeetrootFry"
+
+        elif result[0][6] == 1:
+            out = "Biriyani"
+        elif result[0][7] == 1:
+            out = "Biscuits"
+        elif result[0][8] == 1:
+            out = "BitterGuardFry"
+        elif result[0][9] == 1:
+            out = "Boiledegg"
+        elif result[0][10] == 1:
+            out = "Bread with Peanutbutter"
+
+        elif result[0][11] == 1:
+            out = "BreadandJam"
+        elif result[0][12] == 1:
+            out = "Badam"
+        elif result[0][13] == 1:
+            out = "Burger"
+        elif result[0][14] == 1:
+            out = "CapsicumCurry"
+        elif result[0][15] == 1:
+            out = "Cashew"
+        elif result[0][16] == 1:
+            out = "Chappathi"
+
+        elif result[0][17] == 1:
+            out = "Cheeseballs"
+        elif result[0][18] == 1:
+            out = "ChilliBeef"
+        elif result[0][19] == 1:
+            out = "Chocolate"
+        elif result[0][20] == 1:
+            out = "ChocolateIcecream"
+        elif result[0][21] == 1:
+            out = "ChoolapooriwithChanna"
+        elif result[0][22] == 1:
+            out = "CoffeeorLatte"
+
+        elif result[0][23] == 1:
+            out = "CrabMasala"
+        elif result[0][24] == 1:
+            out = "Cucumber"
+        elif result[0][25] == 1:
+            out = "Curdrice"
+        elif result[0][26] == 1:
+            out = "Dosa"
+
+
+
+        foodName= out
+
+        session["out"]=foodName
+
+        return render_template('Result.html', data=session["out"])
+
+
+
+    else:
+        return render_template('submission.html')
+
+
+
+
+
+
+
+@app.route("/pythonlogin/submission/out", methods=['GET', 'POST'])
+def out():
+    if request.method == "POST":
+        nutrients = {}
+        USDAapiKey = '9f8yGs19GGo5ExPpBj7fqjKOFlXXxkJdMyJKXwG3'
+
+        foodName = session["out"]
+        response = requests.get(
+            'https://api.nal.usda.gov/fdc/v1/foods/search?api_key={}&query={}&requireAllWords={}'.format(USDAapiKey,
+                                                                                                         foodName,
+                                                                                                         True))
+
+        data = json.loads(response.text)
+        concepts = data['foods'][0]['foodNutrients']
+        arr = ["Sugars", "Energy", "Vitamin A", "Vitamin D", "Vitamin B", "Vitamin C", "Protein", "Fiber", "Iron",
+               "Magnesium",
+               "Phosphorus", "Cholestrol", "Carbohydrate", "Total lipid (fat)", "Sodium", "Calcium", ]
+        for x in concepts:
+            if x['nutrientName'].split(',')[0] in arr:
+                if (x['nutrientName'].split(',')[0] == "Total lipid (fat)"):
+                    nutrients['Fat'] = str(x['value']) + " " + x['unitName']
+                else:
+                    nutrients[x['nutrientName'].split(',')[0]] = str(x['value']) + " " + x['unitName']
+
+        return render_template('display.html', x=foodName, data=nutrients, account=session['username'])
+
+
+
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug = True, port = 30376)
